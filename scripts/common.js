b6b1b57d29964014eece17b761e90260b3674a25
Roman <skrypak1997@gmail.com>
2019-12-27 23:26:55 +0200

    project finished

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,21 @@
 $(window).scroll(function () {
     var st = $(this).scrollTop();
-    $(".bg").css({"transform" :"translate(0%, " + (-st)/5 + "%"});
+    console.log(st);
+    $(".bg1").css({"transform" :"translate(0%, " + (st)/10 + "%"});
+    $(".bg2").css({"transform" :"translate(0%, " + (st-1100)/50 + "%"});
+    $(".bg3").css({"transform" :"translate(0%, " + (st-2350)/20 + "%"});
+
+    if (st > 32) {
+        $('.top-line').css({'position': 'fixed', 'margin-top': '0px'});
+    } else {
+        $('.top-line').css({'position': 'absolute', 'margin-top': '32px'});
+    }
+
+    if(st > 32 & st < 72){
+        $('.top-line').css({'height': + (110-(st-32)) });
+    }
+
+    if(st >=72){
+        $('.top-line').css({'height':'70px'})
+    }
 })
